88082ef407a8757196f04d0db99e368afbe03e39
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-05-19 06:57:18 -0400

    rollback serverless router changes

--- api/serverless.js
+++ api/serverless.js
@@ -10,12 +10,10 @@ import Fastify from 'fastify'
 // Instantiate Fastify with some config
 const app = Fastify({
   logger: true
-})(
-  // Register your application as a normal plugin.
-  import('../dist/src/app')
-).then((module) => {
-  app.register(module.default)
 })
+
+// Register your application as a normal plugin.
+app.register(import('../dist/src/app'))
 
 export default async (req, res) => {
   await app.ready()
